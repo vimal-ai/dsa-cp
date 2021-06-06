94965ef119fb8747a2495684185af0516a6b5366
Vimal Mishra <mivimal786@gmail.com>
2021-06-06 07:06:25 +0530

    Create D.cpp

---
+++ graph/D.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5+2;
+bool vis[N];
+vector<int> adj[N];
+
+bool isCycle(int src, int parent){
+    vis[src] =  1;
+    for(auto x: adj[src]){
+        if(x!=parent){
+            return true;
+        } 
+        return isCycle(x, src);
+    }
+    return false;
+}
+
+int main() {
+    int n, m; cin>>n>>m;
+    vector<int> indeg(n, 0);
+    for(int i=0;i<m;i++){
+        int x,y; cin>>x>>y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+    
+    bool cycle = 0;
+    
+    for(int i=0;i<n;i++){
+        if(!vis[i] and isCycle(i,-1)) cycle = true;
+    }
+    if(cycle) cout<<"Cycle is present";
+    else cout<<"Cycle is not present";
+    
+	return 0;
+}
