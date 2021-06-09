45e662644b2415edfed7e66143dbcaffcb9a0784
Vimal Mishra <mivimal786@gmail.com>
2021-06-09 07:43:06 +0530

    Update AB_recursive.cpp

--- dynamic_programming/AB_recursive.cpp
+++ dynamic_programming/AB_recursive.cpp
@@ -1,25 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> memo = vector<vector<int>>(1002,vector<int>(1002,-1));
+vector<vector<int>> memo = vector<vector<int>>(1002, vector<int>(1e5+2));
 
 bool subsetSum(int arr[], int n, int sum){
-    if(sum == 0) return true;
+    
+    if(sum==0) return true;
     else if(n==0) return false;
     
     if(memo[n][sum] != -1) return memo[n][sum];
     
-    int ans = subsetSum(arr, n-1, sum);
+    bool ans = subsetSum(arr, n-1, sum);
     if(arr[n-1] <= sum) ans = ans || subsetSum(arr, n-1, sum-arr[n-1]);
     return memo[n][sum] = ans;
+    
 }
 
+bool equalPartition(int N, int arr[]){
+    
+    int sum = 0;
+    for(int i=0;i<N;i++) sum+=arr[i];
+    
+    if(!sum%2) return 0;
+    
+    return subsetSum(arr, N, sum/2);
+//     return dp(arr, N, sum/2);
+}
+
+
 int main() {
-    
-    int arr[] = {2,5,7,8,10};
-    int n=5;
-    int sum = 11;
-    cout<<subsetSum(arr, n, sum)<<endl;
-    
+	int arr[] = {1,5,11,5};
+	int n = 4;
+	cout<<equalPartition(n, arr);
+	
+	
 	return 0;
 }
+
