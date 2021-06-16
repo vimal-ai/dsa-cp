ed25036e1f4d6e29ca4bdc23e1f61f6827f65786
Vimal Mishra <mivimal786@gmail.com>
2021-06-16 16:17:16 +0530

    Update mergeList.cpp

--- linked_list/mergeList.cpp
+++ linked_list/mergeList.cpp
@@ -1,3 +1,7 @@
+/*
+merge linked list using extra space (additional linked list)
+*/
+
 SinglyLinkedListNode* mergeLists(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
 
     if(head1==NULL) return head2;
@@ -14,3 +18,22 @@ SinglyLinkedListNode* mergeLists(SinglyLinkedListNode* head1, SinglyLinkedListNo
     } 
     return head;
 }
+
+/*
+merge two linked list with O(1) space (in place)
+*/
+Node* sortedMerge(Node* head1, Node* head2)  
+{  
+    if(head1==NULL) return head2;
+    if(head2==NULL) return head1;
+    
+    if(head1->data < head2->data){
+        head1->next = sortedMerge(head1->next, head2);
+    }else{
+        Node* temp = head2->next;
+        head2->next = head1;
+        head1 = head2;
+        head1->next = sortedMerge(head1->next,temp);
+    }
+    return head1;
+} 
