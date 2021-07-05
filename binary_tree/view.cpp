fa6022b8382d4f8d1ba715ed07e6646b123b6ae7
Vimal Mishra <mivimal786@gmail.com>
2021-07-05 09:21:54 +0530

    Update view.cpp

--- binary_tree/view.cpp
+++ binary_tree/view.cpp
@@ -1,3 +1,5 @@
+
+// right view of binary tree
 void rightView(Node* root){
     if(root==NULL) return;
     
@@ -18,6 +20,7 @@ void rightView(Node* root){
     }
 }
 
+// right view of binary tree
 void leftView(Node* root){
     if(root==NULL) return;
     
@@ -39,43 +42,25 @@ void leftView(Node* root){
     }
 }
 
-void topView(Node* root){
-    if(root==NULL) return;
-    
-    queue<Node*> q;
-    q.push(root);
-    
-    stack<Node*>s;
-    while(!q.empty()){
-        int size = q.size();
-        int count = size-1;
-        while(size--){
-            Node* node = q.front();
-            q.pop();
-            if(count==size) s.push(node);
-            
-            if(node->left) q.push(node->left);
-            if(node->right) q.push(node->right);
-        }
-    }
-    while(s.size()>1){
-        cout<<s.top()->data<<" ";
-        s.pop();
-    }
-    
-    q.push(root);
-    
-    while(!q.empty()){
-        int size= q.size();
+// Top view of binary tree
+    vector<int> topView(Node *root)
+    {
+        vector<int>v;
+        map<int, int>mp;
+        queue<pair<int, Node*>>q;
+        q.push({0, root});
         
-        while(size--){
-            Node* node = q.front();
+        while(!q.empty()){
+            int hd = q.front().first;
+            Node* node = q.front().second;
             q.pop();
-            
-            if(!size) cout<<node->data<<" ";
-            
-            if(node->left) q.push(node->left);
-            if(node->right) q.push(node->right);
+            if(mp.find(hd)==mp.end()) mp[hd] = node->data;
+            if(node->left) q.push({hd-1, node->left});
+            if(node->right) q.push({hd+1, node->right});
         }
+        
+        for(auto i:mp){
+            v.push_back(i.second);
+        }
+        return v;
     }
-}
